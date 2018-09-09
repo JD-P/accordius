d0e48c66fdbd7fb9703e145188d87af6dac780ec
John David Pressman <jd@jdpressman.com>
2018-09-09 00:29:36 -0700

    Define Post and Comment models, begin writing GraphQL resolve functions

--- lw2/models.py
+++ lw2/models.py
@@ -1,6 +1,71 @@
+from datetime import date
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class Post(models.Model):
+    """A post object.
+
+    - id: A 17 character truncated base64-encoded md5 hash.
+    - posted_at: The time at which the post was made available, as opposed
+    to draft creation.
+    - frontpage_date: The time at which the post was put "on the front page",
+    whatever that means in our software.
+    - curated_date: The time at which the post was featured or put in the really
+    good section, whatever that means in our software.
+    - user: The author of the post as a user object.
+    - title: The post's title.
+    - url: A url that's submitted to create a link post, NOT the url of the post
+    on the software instance's website.
+    - base_score: The score of the post.
+    - comment_count: The number of comments on the post.
+    - view_count: How many views the post has gotten since it was published.
+    - meta: Legacy field that says whether the post goes into the 'meta' section,
+    of the website, whatever that means in our software.
+    - af: Legacy field that says whether the post is part of the Alignment Forum,
+    always false.
+    - draft: Whether the post is a draft or not."""
+    id = models.CharField(primary_key=True, max_length=17)
+    posted_at = models.DateField(default=date.today)
+    frontpage_date = models.DateField(null=True, default=None)
+    curated_date = models.DateField(null=True, default=None)
+    user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    title = models.CharField(default="Untitled (this should never appear)",
+                             max_length=250)
+    url = models.URLField(null=True)
+    #slug = models.CharField(max_length=60)
+    base_score = models.FloatField(default=1)
+    comment_count = models.IntegerField(default=0)
+    view_count = models.IntegerField(default=0)
+    meta = models.BooleanField(default=False)
+    af = models.BooleanField(default=False)
+    draft = models.BooleanField(default=True)
+
+class Comment(models.Model):
+    """A comment on a Post. 
+
+    - id: A 17 character truncated base64-encoded md5 hash.
+    - user: A user object representing the comments author
+    - post: The post object on which the comment was made.
+    - parent_comment: If this comment is a reply to another, this field
+    is the comment object representing it. 
+    - posted_at: The time at which the comment was posted.
+    - base_score: The score of the comment object.
+    - body: A markdown text comment body.
+    - af: Legacy field for whether this is the Alignment Forum, always false.
+    - is_deleted: Whether the post has been hidden from public consumption."""
+    id = models.CharField(primary_key=True, max_length=17)
+    user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    post = models.ForeignKey(Post, null=True, on_delete=models.SET_NULL)
+    parent_comment = models.ForeignKey('Comment',
+                                       null=True, on_delete=models.SET_NULL)
+    posted_at = models.DateField(default=date.today)
+    base_score = models.FloatField(default=1)
+    body = models.TextField()
+    af = models.BooleanField(default=False)
+    is_deleted = models.BooleanField()
 
 class Test(models.Model):
     testfield = models.CharField(max_length=50)
--- lw2/schema.py
+++ lw2/schema.py
@@ -1,13 +1,58 @@
 from graphene_django import DjangoObjectType
 import graphene
-from .models import Test
+from django.contrib.auth.models import User
+from .models import Post, Comment
 
-class TestType(DjangoObjectType):
+class UserType(DjangoObjectType):
     class Meta:
-        model = Test
+        model = User
 
+class PostType(DjangoObjectType):
+    class Meta:
+        model = Post
+        
+class CommentType(DjangoObjectType):
+    class Meta:
+        model = Comment
+    
 class Query(object):
-    all_tests = graphene.List(TestType)
+    user = graphene.Field(UserType,
+                          id=graphene.Int(),
+                          username=graphene.String())
+    all_users = graphene.List(UserType)
+    post = graphene.Field(PostType,
+                          id=graphene.String(),
+                          posted_at=graphene.types.datetime.Date(),
+                          frontpage_date = graphene.types.datetime.Date(),
+                          curated_date = graphene.types.datetime.Date(),
+                          userId = graphene.Int())
+    all_posts = graphene.List(PostType)
+    all_comments = graphene.List(CommentType)
 
-    def resolve_all_tests(self, info, **kwargs):
-        return Test.objects.all()
+    def resolve_user(self, info, **kwargs):
+        id = kwargs.get('id')
+        username = kwargs.get('username')
+
+        if id:
+            return User.objects.get(id=id)
+        if username:
+            return User.objects.get(username=username)
+
+        return None
+    
+    def resolve_all_users(self, info, **kwargs):
+        return User.objects.all()
+
+    def resolve_post(self, info, **kwargs):
+        id = kwargs.get('id')
+        
+        if id:
+            return Post.objects.get(id=id)
+
+        return None
+        
+    def resolve_all_posts(self, info, **kwargs):
+        return Post.objects.all()
+
+    def resolve_all_comments(self, info, **kwargs):
+        return Comment.objects.all()
