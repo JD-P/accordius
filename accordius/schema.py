61fd0cb4e86774537f07b0819d6244917e618794
John David Pressman <jd@jdpressman.com>
2018-09-14 22:50:19 -0700

    Add edits so mutations happen to global schema

--- accordius/schema.py
+++ accordius/schema.py
@@ -4,4 +4,7 @@ from lw2 import schema
 class Query(schema.Query, graphene.ObjectType):
     pass
 
-schema = graphene.Schema(query=Query)
+class Mutations(schema.Mutations, graphene.ObjectType):
+    pass
+
+schema = graphene.Schema(query=Query, mutation=Mutations)
