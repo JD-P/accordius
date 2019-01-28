86638050b503efc89ed0c8efc81bb317268bca5d
John David Pressman <jd@jdpressman.com>
2019-01-28 13:55:29 -0800

    Add basic unit tests for search API

--- lw2/tests.py
+++ lw2/tests.py
@@ -1,3 +1,50 @@
 from django.test import TestCase
+from django.test import Client
+from django.contrib.auth.models import User
+from lw2.models import *
+import json
 
 # Create your tests here.
+
+c = Client()
+
+class SearchTestCase(TestCase):
+    def setUp(self):
+        user = User.objects.create_user('testuser', 'jd@jdpressman.com', 'testpassword')
+        post1 = Post.objects.create(id='aaaaaaaaaaaaaaaaa', user=user,
+                                    title='My Fruit Post',
+                                    url=None, slug="test-slug-1",
+                                    base_score=5,
+                                    body="My Apple Orange Mango")
+        post2 = Post.objects.create(id='bbbbbbbbbbbbbbbbb', user=user,
+                                    title='My Animal Post',
+                                    url=None, slug="test-slug-2",
+                                    base_score=12,
+                                    body="My Dog Cat Panda")
+        post1.save()
+        post2.save()
+
+    def test_post_search_basic(self):
+        search = c.get("/api/post_search/?query=Apple")
+        search_data = json.loads(search.content.decode("UTF-8"))
+        self.assertEquals(len(search_data), 1)
+        self.assertEquals(search_data[0]["title"], "My Fruit Post")
+        
+    def test_post_search_and(self):
+        search = c.get('/api/post_search/?query=My+Panda')
+        search_data = json.loads(search.content.decode("UTF-8"))
+        self.assertEquals(len(search_data), 1)
+        self.assertEquals(search_data[0]["title"], "My Animal Post")
+
+    def test_post_search_or(self):
+        search = c.get('/api/post_search/?query=Apple+OR+Panda')
+        search_data = json.loads(search.content.decode("UTF-8"))
+        self.assertEquals(len(search_data), 2)
+        self.assertTrue("My Animal Post" in [post["title"] for post in search_data])
+        self.assertTrue("My Fruit Post" in [post["title"] for post in search_data])
+
+    def test_post_search_not(self):
+        search = c.get('/api/post_search/?query=-Apple')
+        search_data = json.loads(search.content.decode("UTF-8"))
+        self.assertEquals(len(search_data), 1)
+        self.assertEquals(search_data[0]["title"], "My Animal Post")
