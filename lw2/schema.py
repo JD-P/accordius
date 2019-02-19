f6423d0260582f5c72d797a3312375c057d2dfa1
John David Pressman <jd@jdpressman.com>
2019-02-18 22:35:14 -0800

    Fix PostsEdit so return values consistent with PostsNew

--- lw2/schema.py
+++ lw2/schema.py
@@ -326,7 +326,7 @@ class PostsInput(graphene.InputObjectType):
 class PostsUnset(graphene.InputObjectType):
     meta = graphene.Boolean()
     draft = graphene.Boolean()
-    url = graphene.String()   
+    url = graphene.Boolean()  
 
 class PostsNew(graphene.Mutation):
     class Arguments:
@@ -374,6 +374,13 @@ class PostsEdit(graphene.Mutation):
     post = graphene.Field(Post)
     _id = graphene.String(name="_id")
     slug = graphene.String()
+
+    def resolve__id(self, info):
+        return self.post.id
+
+    def resolve_slug(self, info):
+        return self.post.slug
+    
     @staticmethod
     def mutate(root, info, document_id=None, set=None, unset=None):
         if not set:
